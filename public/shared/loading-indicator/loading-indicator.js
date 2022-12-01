7a862b4f87dffd5bfaf1592249a4c128f69aac69
Mohammad Askari (Eric) <ericaskari@gmail.com>
2022-12-01 02:41:16 +0200

    Some Small Changes

--- public/shared/loading-indicator/loading-indicator.js
+++ public/shared/loading-indicator/loading-indicator.js
@@ -3,13 +3,13 @@ export class LoadingIndicator {
         const body = document.getElementsByTagName('body')[0];
         const head = document.getElementsByTagName('head')[0];
         const div = document.createElement('div');
-        div.innerHTML = "<img src='/loading.svg' />";
+        div.innerHTML = "<img src='/assets/loading.svg' />";
         div.classList.add('loading-indicator');
         div.setAttribute('visible', 'false');
 
         const link = document.createElement('link');
         link.rel = 'stylesheet';
-        link.href = '/loading-indicator/loading-indicator.css';
+        link.href = '/assets/loading-indicator/loading-indicator.css';
 
         head.appendChild(link);
         body.appendChild(div);
