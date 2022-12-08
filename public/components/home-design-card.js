136c7ac4395a28fbb3fbb8a10b4b1c426efba426
Mohammad Askari (Eric) <32340808+ericaskari@users.noreply.github.com>
2022-12-08 17:58:32 +0200

    Removed Web component and replaced with template and querySelector. (#10)

---
+++ public/components/home-design-card.js
@@ -0,0 +1,106 @@
+const template = `
+    <div class='design-card-profile'>
+        <div>
+            <img class='var--home-design-card-img-profile-source design-card-profile-image' src='/sample-images/horse.jpg' alt='Profile picture' />
+        </div>
+        <div class='design-card-profile-info-wrap'>
+            <b class='var--home-design-card-name'>SomeOne</b>
+            <small><b class='var--home-design-card-username'>@username</b></small>
+        </div>
+    </div>
+
+    <div class='design-card-image-wrap'>
+        <img class='var--home-design-card-img-source design-card-image' src='/sample-images/flowers.jpg' alt='Flowers' />
+    </div>
+
+    <div class='design-card-panel-text'>
+        <b>SomeOne</b> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad corporis eligendi est
+        non quasi quos voluptatem. Dolor harum pariatur suscipit.
+    </div>
+
+    <div class='design-card-panel'>
+        <div class='design-card-panel-likes'>
+            <div>
+                <button class='var--comment-button design-card-icon-button'>
+                    <i class='fa fa-regular fa-message'></i>
+
+                </button>
+            </div>
+            <div>
+                <button class='var--heart-button design-card-icon-button'>
+                    <i class='var--like-heart-icon fa fa-solid fa-heart'></i>
+                </button>
+                <small><b class='var--like-heart-count'>1000</b></small>
+            </div>
+        </div>
+        <div class='design-card-panel-buy'>
+            <div>
+                <small><b class='var--price'>1000</b></small>
+                <i class='fa fa-solid fa-euro-sign'></i>
+            </div>
+            <button class='var--buy-button'>
+                <i class='fa-solid fa-cart-shopping'></i> <small><b>BUY</b></small>
+            </button>
+        </div>
+    </div>
+`;
+
+export const HomeDesignCardBuilder = ({
+    name,
+    username,
+    imgProfileSource,
+    imgSource,
+    price,
+    likeCount,
+    isLiked,
+    onBuyClick,
+    onHeartClick,
+    onCommentClick
+}) => {
+    const card = document.createElement('div');
+    card.innerHTML = template;
+    card.classList.add('design-card');
+
+    const elements = {
+        commentButton: card.querySelector(`.var--comment-button`),
+        heartButton: card.querySelector(`.var--heart-button`),
+        buyButton: card.querySelector(`.var--buy-button`),
+        price: card.querySelector(`.var--price`),
+        name: card.querySelector(`.var--home-design-card-name`),
+        username: card.querySelector(`.var--home-design-card-username`),
+        imgSource: card.querySelector(`.var--home-design-card-img-source`),
+        imgProfileSource: card.querySelector(`.var--home-design-card-img-profile-source`),
+        likeHeartIconSvg: card.querySelector(`.var--like-heart-icon`),
+        likeHeartCount: card.querySelector(`.var--like-heart-count`)
+    };
+
+    elements.price.innerText = price;
+    elements.name.innerText = name;
+    elements.username.innerText = username;
+    elements.imgSource.setAttribute('src', imgSource);
+    elements.imgProfileSource.setAttribute('src', imgProfileSource);
+
+    if (isLiked) {
+        elements.likeHeartIconSvg.classList.add('fa-solid');
+        elements.likeHeartIconSvg.classList.remove('fa-regular');
+    } else {
+        elements.likeHeartIconSvg.classList.remove('fa-solid');
+        elements.likeHeartIconSvg.classList.add('fa-regular');
+    }
+    elements.likeHeartCount.innerText = likeCount;
+
+    elements.commentButton.addEventListener('click', () => {
+        onCommentClick();
+    });
+    elements.heartButton.addEventListener('click', () => {
+        onHeartClick();
+    });
+    elements.buyButton.addEventListener('click', () => {
+        onBuyClick();
+    });
+    elements.imgSource.addEventListener('dblclick', () => {
+        onHeartClick();
+    });
+
+    return card;
+};
