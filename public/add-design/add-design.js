f55ab6e8081d1549c8e67775ae8ca7f4ce92473b
Ainara Larrañaga <ainara.larranagaflores@metropolia.fi>
2022-12-10 16:41:25 +0200

    modified add-design page

--- public/add-design/add-design.js
+++ public/add-design/add-design.js
@@ -3,3 +3,10 @@ import { AppHeaderBuilder } from '../components/app-header.js';
 window.addEventListener('load', async () => {
     AppHeaderBuilder(document.getElementById('app-header'));
 });
+
+const form = document.querySelector('form'),
+    fileInput = form.querySelector('.file-input');
+
+form.addEventListener('click', () => {
+    fileInput.click();
+});
