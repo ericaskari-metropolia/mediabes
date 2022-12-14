bbb2f51d8003b94497bc3ee2835518dd4cc55630
Ainara Larrañaga <ainara.larranagaflores@metropolia.fi>
2022-12-14 16:25:48 +0200

    update README

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # mediabes
-Node version: v16.16.0
 
+Node version: v16.16.0
 
     npm install
     npm run start
 
-
 # HOW DEPLOYMENT PROCESS CODED:
+
 ```
 1. Login
 
    az login -u USERNAME -p PASSWORD
 
-2. Login 
+2. Login
 
    az acr login --name mediabes
 
@@ -38,3 +38,32 @@ Node version: v16.16.0
    --multicontainer-config-type compose \
    --multicontainer-config-file docker-compose-test.yml
 ```
+
+# Members
+
+-   Eric Askari
+-   Yana Krylova
+-   Ainara Larranaga
+
+# Project description
+
+This is a media sharing web application where designers can share, connect, buy, and sell their designs.
+
+# User stories
+
+-   The user wants to post his designs so more people can see it.
+-   The user wants to sell his designs.
+-   The user wants to buy a design.
+
+# User guide
+
+-   Users must be registered in order to use the application.
+-   When the user opens the app, other users's designs will be displayed. The user will be able to like, comment, and buy the design.
+-   The user will be able to upload a design of his and add description and price.
+-   The user will be able to change his avatar.
+-   The user will be able to change his password.
+-   The user will be able to add card details.
+
+# Mediabes ER Diagram
+
+![Reference Image](/public/assets/sample-images/mediabes.png)
--- public/search/search.js
+++ public/search/search.js
@@ -1,5 +1,5 @@
-import { AppBottomHeaderBuilder } from '../shared/components/app-bottom-header.js';
-import { AppTopHeaderBuilder } from '../shared/components/app-top-header';
+// import { AppBottomHeaderBuilder } from '../shared/components/app-bottom-header.js';
+// import { AppTopHeaderBuilder } from '../shared/components/app-top-header';
 
 ('use strict');
 const apiUrl = 'https://mediabes.nicerock-48a381ae.northeurope.azurecontainerapps.io/search/user/?username=';
@@ -53,5 +53,33 @@ document.addEventListener('click', (event) => {
     console.log('mouse clicked somewhere on the page', event);
 });
 
-AppTopHeaderBuilder(document.getElementById('app-top-header'), user.id);
-AppBottomHeaderBuilder(document.getElementById('app-bottom-header'), user.id);
+// AppTopHeaderBuilder(document.getElementById('app-top-header'), user.id);
+// AppBottomHeaderBuilder(document.getElementById('app-bottom-header'), user.id);
+
+// const userCardTemplate = document.querySelector('[data-user-template]');
+// const userCardContainer = document.querySelector('[data-user-cards-container]');
+// const searchInput = document.querySelector('[data-search]');
+
+// let users = [];
+
+// searchInput.addEventListener('input', (e) => {
+//     const value = e.target.value.toLowerCase();
+//     users.forEach((user) => {
+//         const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value);
+//         user.element.classList.toggle('hide', !isVisible);
+//     });
+// });
+// // fetch('https://mediabes.nicerock-48a381ae.northeurope.azurecontainerapps.io')
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then((res) => res.json())
+//     .then((data) => {
+//         users = data.map((user) => {
+//             const card = userCardTemplate.content.cloneNode(true).children[0];
+//             const header = card.querySelector('[data-header]');
+//             const body = card.querySelector('[data-body]');
+//             header.textContent = user.name;
+//             body.textContent = user.email;
+//             userCardContainer.append(card);
+//             return { name: user.name, email: user.email, element: card };
+//         });
+//     });
